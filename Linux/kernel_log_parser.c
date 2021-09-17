aad5ea4ae14e91bfff6a9952dfbb0854d3441293
dlworms32 <49888128+dlworms32@users.noreply.github.com>
2021-09-17 19:20:50 +0900

    21_09_17

---
+++ Linux/kernel_log_parser.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define true 1
+#define false 0
+#define BUF_SIZE 300
+#define LOG "kernel_log.txt"
+
+void get_kernel_log();
+int print_kernel_log(const char*);
+int print_log(const char*, const char*);
+
+int main()
+{
+	if (print_kernel_log("sd") == 0)
+	{
+		print_kernel_log("syste");
+	}
+	return 0;
+}
+
+void get_kernel_log()
+{
+	char cmd[100]={0,};
+	sprintf(cmd, "dmesg > %s", LOG);
+	system(cmd);
+}
+
+int print_kernel_log(const char* word)
+{
+	get_kernel_log();
+	FILE * fp = fopen(LOG, "r");
+	if (fp == NULL)
+	{
+		printf("파일 없음\n");
+		exit(0);
+	}
+	char log_buf[BUF_SIZE] = {0,};
+	int cnt = 0;
+	while (true)
+	{
+		if (feof(fp) != 0)break;
+		fgets(log_buf, sizeof(char) * BUF_SIZE, fp);
+		log_buf[strlen(log_buf) - 1] = '\0';
+		if (strlen(log_buf) == 0) break;
+		cnt += print_log(log_buf, word);
+	}
+	return cnt;
+}
+
+int print_log(const char* buf, const char* word)
+{
+	char log_time[100] = {0,};
+	for(int i = 1; i < strlen(buf); i++){
+		if(buf[i] == ']')
+		{
+			memcpy(log_time, &buf[1], sizeof(char) * (i - 1));
+			break;
+		}
+	}
+	if (strlen(log_time) == 0) return 0;
+	//printf("%s\n", log_time);
+	int last_num = log_time[strlen(log_time) - 1] - '0';
+	//printf("%d\n", last_num);
+	if (last_num % 2 == 0 && strstr(buf, word) != NULL)
+	{
+		printf("%s\n", buf);
+		return 1;
+	}
+	return 0;
+}
---
+++ Linux/mycom_info.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define true 1
+#define false 0
+#define CPUINFO "cpu.txt"
+#define MEMINFO "mem.txt"
+
+void get_cpu_info();
+void get_mem_info();
+void print_cpu_info();
+void print_mem_info();
+float KBtoMiB(int);
+
+int main()
+{
+	print_cpu_info();
+	print_mem_info();
+	return 0;
+}
+
+void get_cpu_info()
+{	
+	char cmd[100] = {0,};
+	sprintf(cmd, "cat /proc/cpuinfo | grep \"cpu MHz\" > %s", CPUINFO);
+	system(cmd);
+}
+
+void get_mem_info()
+{
+	char cmd[100] = {0,};
+	sprintf(cmd, "cat /proc/meminfo | grep Mem > %s", MEMINFO);
+	system(cmd);
+}
+
+void print_cpu_info()
+{
+	FILE * fp = fopen(CPUINFO, "r");
+	if (fp == NULL)
+	{
+		get_cpu_info();
+		fp = fopen(CPUINFO, "r");
+	}
+	int core_cnt = 0;
+	printf("CPU clock \n");
+	while(true)
+	{
+		if (feof(fp) != 0) break;
+		char c1[100] = {0,};
+		char c2[100] = {0,};
+		char c3[100] = {0,};
+		char c4[100] = {0,};
+		fscanf(fp, "%s %s %s %s", c1, c2, c3, c4);
+		if(strlen(c1) == 0 || strlen(c2) == 0 || strlen(c3) == 0 || strlen(c4) == 0) break;
+		printf("core %d : %s %s\n", core_cnt++, c4, c2);
+	}
+	printf("-------------------------------\n");
+}
+
+void print_mem_info()
+{
+	FILE * fp = fopen(MEMINFO, "r");
+	if (fp == NULL)
+	{
+		get_mem_info();
+		fp = fopen(MEMINFO, "r");
+	}
+	printf("Memory info\n");
+	while(true)
+	{
+		if (feof(fp) != 0) break;
+		char info[100] = {0,};
+		int value = 0;
+		char unit[100] = {0,};
+		fscanf(fp, "%s %d %s", info, &value, unit);
+		if(strlen(info) == 0 || strlen(unit) == 0) break;
+		printf("%s %.4lf MiB\n", info, KBtoMiB(value));
+	}
+}
+
+float KBtoMiB(int value)
+{
+	return value / 1049.;
+}
