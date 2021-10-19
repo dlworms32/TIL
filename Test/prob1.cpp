66d0da7e4fe5476428fb3e74031fd45db354c32f
dlworms32 <49888128+dlworms32@users.noreply.github.com>
2021-10-19 18:08:03 +0900

    모의 A 형

---
+++ Test/prob1.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <cstring>
+#include <vector>
+#define MAX 50
+using namespace std;
+
+int N;
+int cnt = 0; // 잡을 수 있는 장기알
+// 방향 북 남 서 동
+int dx[] = { -1, 1, 0, 0 };
+int dy[] = { 0, 0, -1, 1 };
+int board[MAX][MAX];
+bool catched[MAX][MAX];
+
+pair<int, int> used[3];
+
+void print_board() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << board[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
+// 현재 위치에서 갈 수 있는 위치
+// c_x, c_y : 현재 좌표
+// d : 방향
+vector<pair<int, int>> get_enable_pos(int cx, int cy, int d) {
+	vector<pair<int, int>> enable_pos;
+	int nx = cx + dx[d];
+	int ny = cy + dy[d];
+
+	int cnt = 0; // 지나친 알 개수 확인
+	while (true) {
+		if (nx < 0 || nx >= N || ny < 0 || ny >= N) break;
+		if (cnt >= 2) break;
+		if (cnt == 0 && board[nx][ny] == 1) { // 첫번째 알 마주침
+			cnt++;
+		}
+		else if (cnt == 1 && board[nx][ny] == 0) {
+			enable_pos.push_back({ nx,ny });
+		}
+		else if (cnt == 1 && board[nx][ny] == 1) {
+			enable_pos.push_back({ nx, ny });
+			cnt++;
+		}
+		nx = nx + dx[d];
+		ny = ny + dy[d];
+	}
+	return enable_pos;
+}
+
+// 완탐
+void dfs(int step, int cx, int cy)
+{
+	if (step == 3) {
+		//for (int i = 0; i < 3; i++) {
+		//	cout << "step : " << i + 1 << ", (" << used[i].first << ", " << used[i].second << ")\n";
+		//}
+		//print_board();
+		//cout << cnt << "\n";
+		return;
+	}
+	int temp_board[MAX][MAX] = { 0, };
+	memcpy(temp_board, board, sizeof(board));
+	for (int i = 0; i < 4; i++) {
+		vector<pair<int, int>> ep = get_enable_pos(cx, cy, i);
+		for (int p = 0; p < ep.size(); p++) {
+			int nx = ep[p].first;
+			int ny = ep[p].second;
+			if (board[nx][ny] == 1 && !catched[nx][ny]) {
+				cnt++;
+				catched[nx][ny] = true;
+				board[nx][ny] = 0;
+			}
+			used[step] = { nx, ny };
+			dfs(step + 1, ep[p].first, ep[p].second);
+			memcpy(board, temp_board, sizeof(board));
+		}
+	}
+}
+
+int prob1()
+{
+	freopen_s(new FILE*, "sample1.txt", "r", stdin);
+
+	int T; cin >> T;
+	for (int t = 0; t < T; t++) {
+		cin >> N;
+		cnt = 0;
+		int p_x, p_y;
+		memset(board, 0, sizeof(board));
+		memset(catched, 0, sizeof(catched));
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				cin >> board[i][j];
+				if (board[i][j] == 2) {
+					p_x = i;
+					p_y = j;
+					board[i][j] = 0;
+				}
+			}
+		}
+		dfs(0, p_x, p_y);
+		cout << "#" << t + 1 << " " << cnt << "\n";
+	}
+	return 0;
+}
---
+++ Test/prob2.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+#define MAX 20
+using namespace std;
+
+int N = 10;
+int max_value = 0;
+int cnt;
+int station[MAX];
+bool used[MAX];
+
+// 노선 1, 노선 1, 노선 2, 노선 2 의 인덱스
+int comb[4] = { 0, 6, 2, 4 }; // 
+
+bool get_cond1_result() {
+	bool a = false; // 노선 2의 역 1 이 노선 1의 범위에 있는 경우
+	bool b = false;
+
+	int std_st = min(comb[0], comb[1]);
+	int std_ed = max(comb[0], comb[1]);
+	if (std_st < comb[2] && comb[2] < std_ed) a = true;
+	if (std_st < comb[3] && comb[3] < std_ed) b = true;
+
+	// 노선 2의 역이 하나만 노선 1의 범위 내 있는 경우
+	return !(a ^ b);
+}
+
+bool get_cond2_result() {
+	if (comb[0] == (comb[1] + 1) % N || comb[0] == (comb[1] + N - 1) % N) return false;
+	if (comb[2] == (comb[3] + 1) % N || comb[2] == (comb[3] + N - 1) % N) return false;
+	return true;
+}
+
+bool get_cond3_result() {
+	int std_index = comb[0];
+	if ((comb[2] + 1 % N) == std_index || (comb[2] + N - 1) % N == std_index) return false;
+	if ((comb[3] + 1 % N) == std_index || (comb[3] + N - 1) % N == std_index) return false;
+
+	std_index = comb[1];
+	if ((comb[2] + 1 % N) == std_index || (comb[2] + N - 1) % N == std_index) return false;
+	if ((comb[3] + 1 % N) == std_index || (comb[3] + N - 1) % N == std_index) return false;
+	return true;
+}
+
+int calc_value() {
+	int A = station[comb[0]] + station[comb[1]];
+	int B = station[comb[2]] + station[comb[3]];
+
+	return A * A + B * B;
+}
+
+void dfs(int level) {
+	if (level == 4) {
+		if (!get_cond1_result()) return;
+		if (!get_cond2_result()) return;
+		if (!get_cond3_result()) return;
+		max_value = max(max_value, calc_value());
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		if (!used[i]) {
+			used[i] = true;
+			comb[level] = i;
+			dfs(level + 1);
+			used[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	freopen_s(new FILE*, "sample2.txt", "r", stdin);
+	int T; cin >> T;
+	for (int t = 0; t < T; t++) {
+		cout << "#" << t + 1 << " ";
+		cin >> N;
+		memset(station, 0, sizeof(station));
+		max_value = 0;
+		for (int i = 0; i < N; i++) cin >> station[i];
+		dfs(0);
+		cout << max_value << "\n";
+	}
+	return 0;
+}
